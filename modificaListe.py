ee2da20cba0870dc30a2041dcbc1ac14a3eaacec
Paolo Rampino <liadserv@gmail.com>
2024-06-12 10:58:59 +0200

    up

--- modificaListe.py
+++ modificaListe.py
@@ -10,3 +10,15 @@ print(simple)
 simple.append("Chi c'e' c'e'!")
 
 print(simple)
+
+simple.insert(0, "Questo e' il gioco")
+
+print(simple)
+
+simple.insert(5, "...")
+
+print(simple)
+
+del simple[5]
+
+print(simple)
