2dab3483e388f664dff08d8b10c719c4f63bdd90
Paolo Rampino <liadserv@gmail.com>
2024-06-12 12:06:18 +0200

    up

--- funzioni.py
+++ funzioni.py
@@ -8,10 +8,15 @@ stampaqui("Pinco", "Pallino")
 # e' anteporre f ai doppi apici ed inserire le stringhe con {string}
 
 def tichiami(nome, cognome):
-    return f"ti chiami {nome}, {cognome}"
+    return f"ti chiami {nome} {cognome}"
 
 nuova_stringa = tichiami("Pinco", "Pallino")
 print(nuova_stringa)
+
+# in python i nomi degli argomenti posso essere richiamati in modo 
+# non posizionale ovvero argomento=valore
+
+print(tichiami(cognome="Labarca", nome="Remo"))
 
 # alcuni usano ''' o """ per fare commenti
 # di fatto sono dichiarazioni di stringe multiriga
