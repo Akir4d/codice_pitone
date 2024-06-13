1780f00f2e595549e76f30357a87e5580c6c0b4e
Paolo Rampino <liadserv@gmail.com>
2024-06-13 10:40:34 +0200

    up

--- portScanner.py
+++ portScanner.py
@@ -9,14 +9,18 @@ highport = int(portrange.split("-")[1])
 print(f"Verra' scansito da {lowport} a {highport}")
 
 closedPort = []
-
+filteredPort = []
 for port in range(lowport, highport+1):
     s = so.socket(so.AF_INET, so.SOCK_STREAM)
     status = s.connect_ex((target, port))
     if(status == 0):
         print(f"*** Porta {port} - Aperta ***")
     else:
-        closedPort.append(port)
+        if(status == 111):
+            closedPort.append(port)
+        else:
+            filteredPort.append(port)
     s.close()
+print(filteredPort)
 yesno = input("\nVuoi che ti mostri le porte chiuse? (S/N): ")
-if(yesno[0] == "S"): print(f"\ntrovate chiuse {closedPort}")
+if(yesno.startswith("S")): print(f"\ntrovate chiuse {closedPort}")
