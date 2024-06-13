a89a20043641eef0d4aceafe564405acb42088c9
Paolo Rampino <liadserv@gmail.com>
2024-06-13 12:24:11 +0200

    up

--- bruteforce_http.py
+++ bruteforce_http.py
@@ -7,5 +7,7 @@ user_list = username_file.readlines()
 pwd_list = password_file.readlines()
 
 for user in user_list:
+    user =  user.rstrip()
     for pwd in pwd_list:
+        pwd = pwd.rstrip()
         print(f"{user} - {pwd}")
